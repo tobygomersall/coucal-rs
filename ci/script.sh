9aa577af9a46932abae881ad844dff98dc569b15
Toby Gomersall <toby.gomersall@smartacoustics.co.uk>
2022-04-14 17:37:17 +0100

    [ENH] Removed the last of the inline-asm remnants. Added the inline-asm feature even though it is not used to ensure backwards compatibility.

--- ci/script.sh
+++ ci/script.sh
@@ -4,8 +4,4 @@ set -euxo pipefail
 
 if [ -n "${TARGET:-}" ]; then
     cargo check --target $TARGET
-
-    if [ $TRAVIS_RUST_VERSION = nightly ]; then
-        cargo check --target $TARGET --features inline-asm
-    fi
 fi
